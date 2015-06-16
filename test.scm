3bc873b5ffe5176e351919b2942f3ffa6b92fcdb
Tomonaru Kazahaya <tomonacci@gmail.com>
2015-06-16 23:17:06 +0900

    Store loop in watcher

--- test.scm
+++ test.scm
@@ -12,7 +12,28 @@
 (test* "ev-loop-new" #t (is-a? (ev-loop-new 0) <ev-loop>))
 
 (test* "<ev-watcher>" #t (is-a? <ev-watcher> <class>))
-(test* "make <ev-io>" #t (is-a? (make <ev-io>) <ev-io>))
+
+(test-section "<ev-io>")
+(test* "<ev-io> is a subclass of <ev-watcher>" <ev-watcher> (car (member <ev-watcher> (class-direct-supers <ev-io>))))
+(test* "make (minimal)" #t (is-a? (make <ev-io>) <ev-io>))
+(test* "make (:fd only)" ":fd and :events must be supplied together, but got only :fd" (guard (e (else (~ e'message))) (make <ev-io> :fd 0)))
+(test* "make (:events only)" ":fd and :events must be supplied together, but got only :events" (guard (e (else (~ e'message))) (make <ev-io> :events 0)))
+(test* "make (full)" <ev-io>
+  (class-of (make <ev-io> :loop (ev-default-loop 0) :callback (^(watcher revents)) :fd 0 :events EV_READ)))
+(test* "callback (r/w slot)" #t
+  (let1 watcher (make <ev-io>)
+    (set! (~ watcher'callback) (^(watcher revents)))
+    (procedure? (~ watcher'callback))
+    ))
+(test* "loop (ro slot)" #t
+  (let1 watcher (make <ev-io> :loop (ev-default-loop 0))
+    (and
+      (is-a? (~ watcher'loop) <ev-loop>)
+      (guard (e (else #t))
+        (set! (~ watcher'loop) (ev-default-loop 0))
+        #f
+        ))))
+
 (test* "make <ev-timer>" #t (is-a? (make <ev-timer>) <ev-timer>))
 (test* "ev-watcher-active? (inactive)" #f (ev-watcher-active? (make <ev-timer>)))
 (test* "ev-watcher-active? (active)" #t
