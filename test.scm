e12a53ce5610286b9b6ad0b09a7a6deff65192c1
Tomonaru Kazahaya <tomonacci@gmail.com>
2015-06-21 21:05:09 +0900

    Reorder tests

--- test.scm
+++ test.scm
@@ -7,14 +7,28 @@
 
 (test-start "control.libev")
 (use control.libev)
-(test-module 'control.libev)
 
+(test-section "module")
+(test-module 'control.libev)
+(test* "constants" #t (and (integer? EV_READ) (integer? EV_WRITE)))
+
+(test-section "ev-loop")
 (test* "ev-default-loop" #t (is-a? (ev-default-loop 0) <ev-loop>))
 (test* "ev-loop-new" #t (is-a? (ev-loop-new 0) <ev-loop>))
 
+(test-section "ev-watcher")
 (test* "<ev-watcher>" #t (is-a? <ev-watcher> <class>))
+(test* "ev-watcher-active? (inactive)" #f (ev-watcher-active? (make <ev-timer>)))
+(test* "ev-watcher-active? (active)" #t
+  (let1 timer (make <ev-timer>)
+    (ev-timer-init timer values 0 0)
+    (ev-timer-start (ev-default-loop 0) timer)
+    (ev-watcher-active? timer)))
+(test* "ev-watcher-clear-pending" 0
+  (let1 timer (make <ev-io> :loop (ev-default-loop 0))
+    (ev-watcher-clear-pending timer)))
 
-(test-section "<ev-io>")
+(test-section "ev-io")
 (test* "<ev-io> is a subclass of <ev-watcher>" <ev-watcher> (car (member <ev-watcher> (class-direct-supers <ev-io>))))
 (test* "make (minimal)" #t (is-a? (make <ev-io>) <ev-io>))
 (test* "make (:fd only)" ":fd and :events must be supplied together, but got only :fd" (guard (e (else (~ e'message))) (make <ev-io> :fd 0)))
@@ -83,6 +97,7 @@
       (ev-io-stop watcher)
       #f)))
 
+(test-section "ev-timer")
 (test* "make <ev-timer>" #t (is-a? (make <ev-timer>) <ev-timer>))
 (test* "ev-timer-set" 0.1
   (let1 watcher (make <ev-timer>)
@@ -105,16 +120,6 @@
     (guard (e (else #t))
       (parameterize ((ev-thread-local-loop (undefined))) (ev-timer-start watcher))
       #f)))
-(test* "ev-watcher-active? (inactive)" #f (ev-watcher-active? (make <ev-timer>)))
-(test* "ev-watcher-active? (active)" #t
-  (let1 timer (make <ev-timer>)
-    (ev-timer-init timer values 0 0)
-    (ev-timer-start (ev-default-loop 0) timer)
-    (ev-watcher-active? timer)))
-(test* "ev-watcher-clear-pending" 0
-  (let1 timer (make <ev-io> :loop (ev-default-loop 0))
-    (ev-watcher-clear-pending timer)))
-(test* "constants" #t (and (integer? EV_READ) (integer? EV_WRITE)))
 
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
